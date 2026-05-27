e0ade220d3a25d524852827136d67f2854ab0fab
cary0507 <cary0507.zhuang@gmail.com>
2026-05-27 13:11:55 -0400

    Added components for Entity base class to calculate acceleration and velocity components

--- src/main/java/Entity.java
+++ src/main/java/Entity.java
@@ -1,7 +1,14 @@
 public class Entity {
+    // Scalar magnitudes
     private double acceleration;
-    private double velocity;
-    private double maxVelocity;
+    private double speed = 0;
+    private double maxSpeed;
+    private double facingDir;
+    // Vector components
+    private double accX;
+    private double accY;
+    private double velX;
+    private double velY;
     private int x;
     private int y;
     private int hitboxWidth;
@@ -10,12 +17,13 @@ public class Entity {
     private String[] animePaths;  // How it looks when it is moving
     private String iconPath;  // How it looks when at stationary
 
-    public Entity(int x, int y, double maxVelocity, int hitboxWidth, int hitboxHeight) {
+    public Entity(int x, int y, int hitboxWidth, int hitboxHeight, double maxSpeed, double acceleration) {
         this.x = x;
         this.y = y;
         this.hitboxWidth = hitboxWidth;
         this.hitboxHeight = hitboxHeight;
-        this.maxVelocity = maxVelocity;
+        this.maxSpeed = maxSpeed;
+        this.acceleration = acceleration;
     }
 
     public int getX() {
@@ -26,4 +34,23 @@ public class Entity {
         return this.y;
     }
 
+    public void updateAccVectors() {
+        this.accX = Math.cos(this.facingDir) * this.acceleration;
+        this.accY = Math.sin(this.facingDir) * this.acceleration;
+    }
+
+    public void updateVelVectors() {
+        double newVelX = this.velX + this.accX;
+        double newVelY = this.velY + this.accY;
+        double velMagnitude = Math.sqrt(newVelX * newVelX + newVelY * newVelY);
+        if (velMagnitude > this.maxSpeed) {
+            double maxVelX = Math.cos(this.facingDir) * this.maxSpeed;
+            double maxVelY = Math.sin(this.facingDir) * this.maxSpeed;
+            this.velX = maxVelX;
+            this.velY = maxVelY;
+        } else {
+            this.velX = newVelX;
+            this.velY = newVelY;
+        }
+    }
 }
--- src/main/java/Mountable.java
+++ src/main/java/Mountable.java
@@ -1,7 +1,5 @@
 public class Mountable extends Entity {
     private boolean isMounted;  // Whether the mount is being ridden or on the ground
-    private String[] animePaths;  // How it looks when mount is on the ground
-    private String iconPath;  // How it looks when mount being ridden
 
     public Mountable(int x, int y, double maxVelocity, int hitboxWidth, int hitboxHeight) {
         super(x, y, maxVelocity, hitboxWidth, hitboxHeight);
